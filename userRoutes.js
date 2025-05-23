278339d459132136f521da910a461b7f33f3819b
VARSHINI-IT <varshiitr119@gmail.com>
2025-05-23 11:13:02 +0530

    Add files via upload

--- userRoutes.js
+++ userRoutes.js
@@ -1,15 +1,17 @@
 import express from "express";
+import { register } from "../controller/userController.js";
 
 const userRoutes = express.Router();
 
 //GET
 
 //POST
+userRoutes.post("/register",register)
 
-userRoutes.post("/register",(req,res) => {
-    const {name} = req.body;
-    res.status(200).json({data:`Welcome ${name}`})
-})
+//userRoutes.post("/register",(req,res) => {
+    //const {name} = req.body;
+    //res.status(200).json({data:`Welcome ${name}`})
+//})
 
 //PUT
 
